23b65b1d04dad6a707a9f845ffaa3cc1860d7d85
thorronnie <cyriltiphaine@outlook.com>
2023-10-17 15:58:42 +0200

    premier.volet

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/* commande alerte côté client */
+
+alert("coucou");
+console.log("c'est jolie");
+prompt("veuillez écrire");
+confirm("Merci")
+
+
+
+
+var nom= prompt("Saisissez votre nom");
+alert(nom);
+var prénom= prompt("Saisissez votre prénom");
+alert(prénom);
+
+var nb1= window.prompt("entrez un nombre");
+var nb2= window.prompt("entrez un deuxième nombre");
+alert(nb1*nb2);
+
+var C= window.prompt("entrez la température en degrés");
+var F=(C*9/5)+32;
+alert("la température en Fahrenheit est de "+F);
