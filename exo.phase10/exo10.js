ad342b0e27642112ad3d94a8d15273ab0d149973
thorronnie <cyriltiphaine@outlook.com>
2023-10-20 10:48:11 +0200

    exo.phase10

---
+++ exo.phase10/exo10.js
@@ -0,0 +1,5 @@
+var x=0;
+var y=0;
+
+x = parseInt (window.prompt("Entrez votre premier nombre"));
+y = parseInt (Window.prompt("Entrez votre deuxieme nombre"));
--- exo.phase9/exo9bis.js
+++ exo.phase9/exo9bis.js
@@ -1,28 +1,31 @@
-// var n = parseInt(window.prompt("Entrez un nombre"));
+// /*exo1*/
+// // var n = parseInt(window.prompt("Entrez un nombre"));
 
-// for(n; n>=0; --n){
-//     i= n;
-//     alert(i);
-//     console.log(i);
+// // for(n; n>=0; --n){
+// //     i= n;
+// //     alert(i);
+// //     console.log(i);
+// // }
+
+// /*exo3*/
+// var i=true;
+// var somme =0;
+// var moy =0;
+// var counter=0;
+
+// while(i==true){
+//     var saisie= parseInt(window.prompt("Saisir un nombre entier"));
+//     console.log(saisie);    
+//     if (saisie==0){
+//         i=false;
+
+// else{
+//     somme= somme+saisie;
+//     ++counter;
 // }
+// }
+// var moy = somme/counter
+// alert("La somme est de :"+somme+
+// "Le nombre de saisie est de :"+counter+
+// "la moyenne est de :"+moy);
 
-var i=true;
-var somme =0;
-var moy =0;
-var counter=0;
-
-while(i==true){
-    var saisie= parseInt(window.prompt("Saisir un nombre entier"));
-    console.log(saisie);    
-    if (saisie==0){
-        i=false;
-}
-else{
-    somme= somme+saisie;
-    ++counter;
-}
-}
-var moy = somme/counter
-alert("La somme est de :"+somme+
-"Le nombre de saisie est de :"+saisie+
-"la moyenne est de :"+moy)
