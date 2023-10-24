bda7575dbc77e0a3b35bcafc0bfb56a4e704296e
thorronnie <cyriltiphaine@outlook.com>
2023-10-24 12:07:58 +0200

    modif.contact

---
+++ evaluation_js/jarditou_js/assets/js/exo5.js
@@ -0,0 +1,25 @@
+var submit = document.getElementById('btn_envoi');
+var nom = document.getElementById('nom');
+var nom_m = document.getElementById('nom_manquant');
+
+var prenom = document.getElementById("prenom");
+var prenom_m = document.getElementById("prenom_manquant");
+
+validation.addEventListener('click',f_valid);
+    
+    function f_valid(e){
+        if (nom.validity.valueMissing) {
+            e.preventDefault();
+            nom_m.textContent ='Nom manquant';
+            nom_m.style.color = 'red';
+            }
+    
+
+        else if (prenom.validity.valueMissing){
+        e.preventDefault();
+        prenom_m.textContent = 'Prénom manquant';
+        prenom_m.style.color = 'red';
+    }
+   
+}
+
